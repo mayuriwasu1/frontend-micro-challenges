0c8760f14cb0aab2fd027dadf15eb6a1234cf2f8
Mayuri <mayuriwasu2000@gmail.com>
2022-01-26 18:16:07 +0530

    final update

--- carousels/index.js
+++ carousels/index.js
@@ -57,4 +57,56 @@ function brandSlidera() {
   
   brandSlidera();
   
+  //the other slider with info
+
   
+function productSliders() {
+    var slideList = document.querySelector(".sliderP")
+  
+    var count = 1
+    var click = 2
+    var size = document.querySelector(".sliderP").offsetWidth
+    if (size < 800) {
+      click = 4
+    }
+    if (count == 1) {
+      document.getElementById("swipe_L").style.opacity = 0.5
+      document.getElementById("swipe_L").style.display = "hidden"
+    }
+    console.log(size)
+    function moveToright() {
+      if (count == 2) {
+        document.getElementById("swipe_L").style.opacity = 0.5
+      }
+      if (count > 1) {
+        count = count - 2
+        slideList.style.left = "-" + count * 31 + "rem"
+  
+        document.getElementById("swipe_R").style.opacity = 1
+  
+        count++
+      } else {
+        document.getElementById("swipe_L").style.opacity = 0.5
+        document.getElementById("swipe_L").style.display = "hidden"
+      }
+    }
+    function moveToleft() {
+      if (count == click - 1) {
+        document.getElementById("swipe_R").style.opacity = 0.5
+        document.getElementById("swipe_R").style.display = "hidden"
+      }
+      if (count < click) {
+        slideList.style.left = "-" + count * 31 + "rem"
+  
+        document.getElementById("swipe_L").style.opacity = 1
+        count++
+      } else {
+        document.getElementById("swipe_R").style.opacity = 0.5
+        document.getElementById("swipe_R").style.display = "hidden"
+      }
+    }
+    document.getElementById("swipe_L").addEventListener("click", moveToright)
+    document.getElementById("swipe_R").addEventListener("click", moveToleft)
+  }
+  
+  productSliders()
